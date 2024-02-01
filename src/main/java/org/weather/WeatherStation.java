6ef4c7d21bd77c958559b74f8ed986360246c2c8
martingega <martin.gega@studenti.unimi.it>
2024-02-01 14:04:29 +0100

    implemented Statistics display

--- src/main/java/org/weather/WeatherStation.java
+++ src/main/java/org/weather/WeatherStation.java
@@ -2,6 +2,7 @@ package org.weather;
 
 import org.weather.displays.CurrentConditionsDisplay;
 import org.weather.displays.ForecastDisplay;
+import org.weather.displays.StatisticsDisplay;
 
 public class WeatherStation {
 
@@ -13,9 +14,13 @@ public class WeatherStation {
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
         // Create the Forecast display and pass it the WeatherData object
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        // Create the Statistics display and pass it the WeatherData object
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         // Simulate new weather measurements
         weatherData.setMeasurements(80, 65, 30.4);
+        System.out.println("------------------------");
         weatherData.setMeasurements(82, 70, 29.2);
+        System.out.println("------------------------");
         weatherData.setMeasurements(78, 90, 29.2);
     }
 }
--- src/main/java/org/weather/displays/StatisticsDisplay.java
+++ src/main/java/org/weather/displays/StatisticsDisplay.java
@@ -1,22 +1,44 @@
 package org.weather.displays;
 
+import org.weather.Observer;
+import org.weather.Subject;
+
 /**
  * This display element keeps track of the minimium/average/maximum measurements and displays them
  */
-public class StatisticsDisplay implements DisplayElement {
+public class StatisticsDisplay implements Observer, DisplayElement {
 
-    private double temperature;
-    private double humidity;
-    private double pressure;
+    private double maxTemperature = 0.0;
+    private double minTemperature = 300;
+    private double temperatureSum = 0.0;
+    private int numReadings;
+    private Subject weatherData;
+
+    public StatisticsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
 
     public void update(double temperature, double humidity, double pressure) {
-        this.temperature = temperature;
-        this.humidity = humidity;
-        this.pressure = pressure;
+        temperatureSum += temperature;
+        numReadings++;
+        // calcolo della temperatura massima
+        if (temperature > maxTemperature) {
+            maxTemperature = temperature;
+        }
+        // calcolo della temperatura minima
+        if (temperature < minTemperature) {
+            minTemperature = temperature;
+        }
+        display();
     }
 
     @Override
     public void display() {
         // display the average, min and max measurements
+        double avgTemperature = temperatureSum / numReadings;
+        System.out.println("Average temperature: " + avgTemperature);
+        System.out.println("Maximum temperature: " + maxTemperature);
+        System.out.println("Minimum temperature: " + minTemperature);
     }
 }
