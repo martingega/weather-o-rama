3d89cfbd1fd3c4e92451370bee81b50f20718e03
martingega <martin.gega@studenti.unimi.it>
2024-02-01 01:17:49 +0100

    implemented the Subject interface in WeatherData

--- src/main/java/org/weather/WeatherData.java
+++ src/main/java/org/weather/WeatherData.java
@@ -4,25 +4,24 @@ import org.weather.displays.CurrentConditionsDisplay;
 import org.weather.displays.ForecastDisplay;
 import org.weather.displays.StatisticsDisplay;
 
+import java.util.ArrayList;
+
 /**
  * The WeatherData object tracks the data coming
  * from the Weather Station and updates the displays
  */
-public class WeatherData {
+public class WeatherData implements Subject {
 
-    public double temperature;
-    public double humidity;
-    public double pressure;
+    private ArrayList<Observer> observers; // we add an ArrayList to hold the Observers and we create it in the constructor
 
-    CurrentConditionsDisplay currentConditionsDisplay;
-    StatisticsDisplay statisticsDisplay;
-    ForecastDisplay forecastDisplay;
+    private double temperature;
+    private double humidity;
+    private double pressure;
 
-    public WeatherData(CurrentConditionsDisplay ccd, StatisticsDisplay sd, ForecastDisplay fd){
-        currentConditionsDisplay = ccd;
-        statisticsDisplay = sd;
-        forecastDisplay = fd;
+    public WeatherData() {
+        observers = new ArrayList<>(); // we create the observers ArrayList here in the constructor
     }
+
     // returns the most recent weather measurement for temperature
     public double getTemperature(){
         return this.temperature;
@@ -39,20 +38,41 @@ public class WeatherData {
     }
 
     /*
-     * This method gets called whenever the weather
-     * measurements have been updated
+     * This method gets called whenever the weather measurements have been updated.
+     * We notify the observers when we get updated measurements from the Weather Station
      */
     public void measurementsChanged(){
-
-        // grab the most recent measurements by calling the WeatherData's getter methods
-        double temp = getTemperature();
-        double humidity = getHumidity();
-        double pressure = getPressure();
-
-        // now update the three displays, call each display element to update its dsiplay passing it the most recent measurements
-        currentConditionsDisplay.update(temperature, humidity, pressure);
-        statisticsDisplay.update(temperature, humidity, pressure);
-        forecastDisplay.update(temperature, humidity, pressure);
+        notifyObservers();
     }
 
+    /*
+     * Rather than reading actual weather data off a device we're going to use this setter method
+     * to test our display elements. For fun we could write code to grab measurements elsewhere
+     */
+    public void setMeasurements(double temperature, double humidity, double pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+    @Override
+    public void registerObserver(Observer obs) {
+        observers.add(obs);
+    }
+
+    @Override
+    public void removeObserver(Observer obs) {
+        observers.remove(obs);
+    }
+
+    /*
+     * This is where we tell all the observers about the state.
+     * Because they are all Observers, we know they will implement update()
+     * so we know how to notify them.
+     */
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers)
+            observer.update(getTemperature(), getHumidity(), getPressure());
+    }
 }
